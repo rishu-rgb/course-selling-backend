352013b577f1749102065089800c64938dce8600
rishu-rgb <rishuraj7662.m@gmail.com>
2026-07-29 08:54:58 +0530

    added admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,53 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel} = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD_ADMIN = "rishu12345"
+
 //brcypt, zod, jsonwebtoken
 
 
-adminRouter.post("/signup",function(req, res){
+adminRouter.post("/signup", async function(req, res){
+     const{ email, password, firstName, lastName } =req.body; // todo; adding zod validation
+    // todo: hash the password so plain pass is not stored in the db
+    // todo: use try catch 
+
+    await adminModel.create({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    })
+
     res.json({
-        message: "signup endpoint"
+        message: "signup succeeded"
     })
 })
 
-adminRouter.post("/signin",function(req, res){
-    res.json({
-        message: "signin endpoint"
-    })
+adminRouter.post("/signin", async function(req, res){
+      const {email ,password } = req.body;
+     // todo: ideally password should be hashed, and hence we cannot compare the user provided password and thee database password
+     const admin = await adminModel.findOne({
+        email: email,
+        password: password
+     });
+//.find will not work as it return an array for all the user with this credentials which is true in all the cases as it will always return empty array
+// if want to use find then check user.length === 1
+     if(admin) {
+        const token = jwt.sign({
+            id: admin._id
+        }, JWT_USER_PASSWORD_ADMIN);
+
+        //do cookie logic
+
+        res.json({
+            token: token
+        })
+     } else{
+        res.status(403).json({
+        message: "incorrect credentials"
+        })
+     }
 })
 
 // /api/v1/course/
